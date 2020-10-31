0ba940b737ff40718601a64126450c3d65a37913
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-01 04:53:04 +0700

    #15 - Add registerNewUser service in UserServiceImpl in Backend Service

--- backend/src/main/java/com/hendisantika/userservice/service/UserServiceImpl.java
+++ backend/src/main/java/com/hendisantika/userservice/service/UserServiceImpl.java
@@ -1,10 +1,17 @@
 package com.hendisantika.userservice.service;
 
+import com.hendisantika.userservice.dto.SignUpRequest;
+import com.hendisantika.userservice.entity.User;
+import com.hendisantika.userservice.exception.UserAlreadyExistAuthenticationException;
 import com.hendisantika.userservice.repository.RoleRepository;
 import com.hendisantika.userservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,4 +33,24 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Override
+    @Transactional(value = "transactionManager")
+    public User registerNewUser(final SignUpRequest signUpRequest) throws UserAlreadyExistAuthenticationException {
+        if (signUpRequest.getUserID() != null && userRepository.existsById(signUpRequest.getUserID())) {
+            throw new UserAlreadyExistAuthenticationException("User with User id " + signUpRequest.getUserID() + " " +
+                    "already exist");
+        } else if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            throw new UserAlreadyExistAuthenticationException("User with email id " + signUpRequest.getEmail() + " " +
+                    "already exist");
+        }
+        User user = buildUser(signUpRequest);
+        Date now = Calendar.getInstance().getTime();
+        user.setCreatedDate(now);
+        user.setModifiedDate(now);
+        user = userRepository.save(user);
+        userRepository.flush();
+        return user;
+    }
+
 }
