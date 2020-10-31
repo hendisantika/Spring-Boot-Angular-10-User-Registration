e8eb2a693e3da12dbab24c0c123e254a61e1c1bb
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-01 04:52:27 +0700

    #14 - Add UserServiceImpl in Backend Service

---
+++ backend/src/main/java/com/hendisantika/userservice/service/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.hendisantika.userservice.service;
+
+import com.hendisantika.userservice.repository.RoleRepository;
+import com.hendisantika.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/11/20
+ * Time: 04.52
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+}
