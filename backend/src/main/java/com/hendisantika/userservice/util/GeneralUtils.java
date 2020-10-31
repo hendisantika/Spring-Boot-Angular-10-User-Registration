f91905ac8733105af71da787d68eb0db3450cb70
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-01 05:00:33 +0700

    #20 - Add GeneralUtils in Backend Service

---
+++ backend/src/main/java/com/hendisantika/userservice/util/GeneralUtils.java
@@ -0,0 +1,49 @@
+package com.hendisantika.userservice.util;
+
+import com.hendisantika.userservice.dto.LocalUser;
+import com.hendisantika.userservice.dto.SocialProvider;
+import com.hendisantika.userservice.dto.UserInfo;
+import com.hendisantika.userservice.entity.Role;
+import com.hendisantika.userservice.entity.User;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/11/20
+ * Time: 04.59
+ */
+public class GeneralUtils {
+
+    public static List<SimpleGrantedAuthority> buildSimpleGrantedAuthorities(final Set<Role> roles) {
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return authorities;
+    }
+
+    public static SocialProvider toSocialProvider(String providerId) {
+        for (SocialProvider socialProvider : SocialProvider.values()) {
+            if (socialProvider.getProviderType().equals(providerId)) {
+                return socialProvider;
+            }
+        }
+        return SocialProvider.LOCAL;
+    }
+
+    public static UserInfo buildUserInfo(LocalUser localUser) {
+        List<String> roles =
+                localUser.getAuthorities().stream().map(item -> item.getAuthority()).collect(Collectors.toList());
+        User user = localUser.getUser();
+        return new UserInfo(user.getId().toString(), user.getDisplayName(), user.getEmail(), roles);
+    }
+}
