65ddf6634928ef289fb972f1cc621deb78da6768
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-02 06:33:05 +0700

    #28 - Add LocalUserDetailService in Backend Service

---
+++ backend/src/main/java/com/hendisantika/userservice/service/LocalUserDetailService.java
@@ -0,0 +1,52 @@
+package com.hendisantika.userservice.service;
+
+import com.hendisantika.userservice.dto.LocalUser;
+import com.hendisantika.userservice.entity.User;
+import com.hendisantika.userservice.exception.ResourceNotFoundException;
+import com.hendisantika.userservice.util.GeneralUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/11/20
+ * Time: 06.31
+ */
+@Service("localUserDetailService")
+public class LocalUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    @Transactional
+    public LocalUser loadUserByUsername(final String email) throws UsernameNotFoundException {
+        User user = userService.findUserByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User " + email + " was not found in the database");
+        }
+        return createLocalUser(user);
+    }
+
+    @Transactional
+    public LocalUser loadUserById(Long id) {
+        User user = userService.findUserById(id).orElseThrow(() -> new ResourceNotFoundException("User", "id", id));
+        return createLocalUser(user);
+    }
+
+    /**
+     * @param user
+     * @return
+     */
+    private LocalUser createLocalUser(User user) {
+        return new LocalUser(user.getEmail(), user.getPassword(), user.isEnabled(), true, true, true,
+                GeneralUtils.buildSimpleGrantedAuthorities(user.getRoles()), user);
+    }
+}
