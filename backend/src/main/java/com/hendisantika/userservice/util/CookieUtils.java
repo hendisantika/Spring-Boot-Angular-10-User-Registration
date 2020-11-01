e19928dbed0b41cd38c4e52a10d6c8d50f08aa64
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-02 06:44:45 +0700

    #38 - Add CookieUtils in Backend Service

---
+++ backend/src/main/java/com/hendisantika/userservice/util/CookieUtils.java
@@ -0,0 +1,30 @@
+package com.hendisantika.userservice.util;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/11/20
+ * Time: 06.44
+ */
+public class CookieUtils {
+    public static Optional<Cookie> getCookie(HttpServletRequest request, String name) {
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(name)) {
+                    return Optional.of(cookie);
+                }
+            }
+        }
+
+        return Optional.empty();
+    }
+}
