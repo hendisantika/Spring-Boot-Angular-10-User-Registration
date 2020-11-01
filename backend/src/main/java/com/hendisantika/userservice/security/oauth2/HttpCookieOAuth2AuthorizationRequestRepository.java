611538435788cc3dd7ab2b5f0405e262d8a824ff
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-02 06:43:49 +0700

    #37 - Add HttpCookieOAuth2AuthorizationRequestRepository in Backend Service

---
+++ backend/src/main/java/com/hendisantika/userservice/security/oauth2/HttpCookieOAuth2AuthorizationRequestRepository.java
@@ -0,0 +1,22 @@
+package com.hendisantika.userservice.security.oauth2;
+
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/11/20
+ * Time: 06.43
+ */
+@Component
+public class HttpCookieOAuth2AuthorizationRequestRepository implements AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
+    public static final String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
+    public static final String REDIRECT_URI_PARAM_COOKIE_NAME = "redirect_uri";
+    private static final int cookieExpireSeconds = 180;
+
+}
