1a41e5741e4a7ff678abeb040291728eb6d81370
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-02 06:37:44 +0700

    #31 - Add loadUser in Backend Service

--- backend/src/main/java/com/hendisantika/userservice/security/oauth2/CustomOAuth2UserService.java
+++ backend/src/main/java/com/hendisantika/userservice/security/oauth2/CustomOAuth2UserService.java
@@ -1,10 +1,17 @@
 package com.hendisantika.userservice.security.oauth2;
 
+import com.hendisantika.userservice.dto.SocialProvider;
 import com.hendisantika.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +30,25 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     @Autowired
     private Environment env;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(oAuth2UserRequest);
+        try {
+            Map<String, Object> attributes = new HashMap<>(oAuth2User.getAttributes());
+            String provider = oAuth2UserRequest.getClientRegistration().getRegistrationId();
+            if (provider.equals(SocialProvider.LINKEDIN.getProviderType())) {
+                populateEmailAddressFromLinkedIn(oAuth2UserRequest, attributes);
+            }
+            return userService.processUserRegistration(provider, attributes, null, null);
+        } catch (AuthenticationException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            // Throwing an instance of AuthenticationException will trigger the
+            // OAuth2AuthenticationFailureHandler
+            throw new OAuth2AuthenticationProcessingException(ex.getMessage(), ex.getCause());
+        }
+    }
+
 }
