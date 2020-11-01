ad01289e52837ea1cf8878bc339d6ae701dcf817
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-02 06:56:46 +0700

    #48 - Add determineTargetUrl in Backend Service

--- backend/src/main/java/com/hendisantika/userservice/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ backend/src/main/java/com/hendisantika/userservice/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -1,16 +1,24 @@
 package com.hendisantika.userservice.security.oauth2;
 
 import com.hendisantika.userservice.config.AppProperties;
+import com.hendisantika.userservice.exception.BadRequestException;
 import com.hendisantika.userservice.security.jwt.TokenProvider;
+import com.hendisantika.userservice.util.CookieUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URI;
+import java.util.Optional;
+
+import static com.hendisantika.userservice.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
 
 /**
  * Created by IntelliJ IDEA.
@@ -51,4 +59,39 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         clearAuthenticationAttributes(request, response);
         getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
+
+    @Override
+    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) {
+        Optional<String> redirectUri =
+                CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME).map(Cookie::getValue);
+
+        if (redirectUri.isPresent() && !isAuthorizedRedirectUri(redirectUri.get())) {
+            throw new BadRequestException("Sorry! We've got an Unauthorized Redirect URI and can't proceed with the " +
+                    "authentication");
+        }
+
+        String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
+
+        String token = tokenProvider.createToken(authentication);
+
+        return UriComponentsBuilder.fromUriString(targetUrl).queryParam("token", token).build().toUriString();
+    }
+
+    protected void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
+        super.clearAuthenticationAttributes(request);
+        httpCookieOAuth2AuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+    }
+
+    private boolean isAuthorizedRedirectUri(String uri) {
+        URI clientRedirectUri = URI.create(uri);
+
+        return appProperties.getOauth2().getAuthorizedRedirectUris().stream().anyMatch(authorizedRedirectUri -> {
+            // Only validate host and port. Let the clients use different paths if they want
+            // to
+            URI authorizedURI = URI.create(authorizedRedirectUri);
+            return authorizedURI.getHost().equalsIgnoreCase(clientRedirectUri.getHost()) && authorizedURI.getPort() == clientRedirectUri.getPort();
+        });
+    }
+
 }
