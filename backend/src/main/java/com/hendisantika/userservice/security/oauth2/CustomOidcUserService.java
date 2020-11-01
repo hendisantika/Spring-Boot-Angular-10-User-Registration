a8e25d834474d912301adcc16bdfea9d4eee9439
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-02 06:43:14 +0700

    #36 - Add CustomOidcUserService in Backend Service

---
+++ backend/src/main/java/com/hendisantika/userservice/security/oauth2/CustomOidcUserService.java
@@ -0,0 +1,44 @@
+package com.hendisantika.userservice.security.oauth2;
+
+import com.hendisantika.userservice.exception.OAuth2AuthenticationProcessingException;
+import com.hendisantika.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/11/20
+ * Time: 06.42
+ */
+@Service
+public class CustomOidcUserService extends OidcUserService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
+        OidcUser oidcUser = super.loadUser(userRequest);
+        try {
+            return userService.processUserRegistration(userRequest.getClientRegistration().getRegistrationId(),
+                    oidcUser.getAttributes(), oidcUser.getIdToken(),
+                    oidcUser.getUserInfo());
+        } catch (AuthenticationException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            // Throwing an instance of AuthenticationException will trigger the
+            // OAuth2AuthenticationFailureHandler
+            throw new OAuth2AuthenticationProcessingException(ex.getMessage(), ex.getCause());
+        }
+    }
+}
