7ff2956f216276ed54c124a8847f64edd5b4c11d
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-02 06:45:21 +0700

    #39 - Add addCookie & deleteCookie in Backend Service

--- backend/src/main/java/com/hendisantika/userservice/util/CookieUtils.java
+++ backend/src/main/java/com/hendisantika/userservice/util/CookieUtils.java
@@ -2,6 +2,7 @@ package com.hendisantika.userservice.util;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
 
 /**
@@ -27,4 +28,27 @@ public class CookieUtils {
 
         return Optional.empty();
     }
+
+    public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
+    }
+
+    public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(name)) {
+                    cookie.setValue("");
+                    cookie.setPath("/");
+                    cookie.setMaxAge(0);
+                    response.addCookie(cookie);
+                }
+            }
+        }
+    }
+
 }
