08a746404ed20af7e0bc8d9fb1a1a40596be06e9
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-02 06:40:06 +0700

    #33 - Add populateEmailAddressFromLinkedIn in Backend Service

--- backend/src/main/java/com/hendisantika/userservice/security/oauth2/CustomOAuth2UserService.java
+++ backend/src/main/java/com/hendisantika/userservice/security/oauth2/CustomOAuth2UserService.java
@@ -1,16 +1,25 @@
 package com.hendisantika.userservice.security.oauth2;
 
 import com.hendisantika.userservice.dto.SocialProvider;
+import com.hendisantika.userservice.exception.OAuth2AuthenticationProcessingException;
 import com.hendisantika.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -49,6 +58,20 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             // OAuth2AuthenticationFailureHandler
             throw new OAuth2AuthenticationProcessingException(ex.getMessage(), ex.getCause());
         }
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public void populateEmailAddressFromLinkedIn(OAuth2UserRequest oAuth2UserRequest, Map<String, Object> attributes) throws OAuth2AuthenticationException {
+        String emailEndpointUri = env.getProperty("linkedin.email-address-uri");
+        Assert.notNull(emailEndpointUri, "LinkedIn email address end point required");
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + oAuth2UserRequest.getAccessToken().getTokenValue());
+        HttpEntity<?> entity = new HttpEntity<>("", headers);
+        ResponseEntity<Map> response = restTemplate.exchange(emailEndpointUri, HttpMethod.GET, entity, Map.class);
+        List<?> list = (List<?>) response.getBody().get("elements");
+        Map map = (Map<?, ?>) ((Map<?, ?>) list.get(0)).get("handle~");
+        attributes.putAll(map);
     }
 
 }
