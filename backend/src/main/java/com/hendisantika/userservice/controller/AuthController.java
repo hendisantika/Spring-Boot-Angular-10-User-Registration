edf5590daafb1f88f75b53528770a84161eefb03
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-03 05:30:42 +0700

    #60 - Add authenticateUser API in AuthController in Backend Service

--- backend/src/main/java/com/hendisantika/userservice/controller/AuthController.java
+++ backend/src/main/java/com/hendisantika/userservice/controller/AuthController.java
@@ -1,12 +1,24 @@
 package com.hendisantika.userservice.controller;
 
+import com.hendisantika.userservice.dto.JwtAuthenticationResponse;
+import com.hendisantika.userservice.dto.LocalUser;
+import com.hendisantika.userservice.dto.LoginRequest;
 import com.hendisantika.userservice.security.jwt.TokenProvider;
 import com.hendisantika.userservice.service.UserService;
+import com.hendisantika.userservice.util.GeneralUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,4 +42,15 @@ public class AuthController {
 
     @Autowired
     private TokenProvider tokenProvider;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        Authentication authentication =
+                authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(),
+                        loginRequest.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = tokenProvider.createToken(authentication);
+        LocalUser localUser = (LocalUser) authentication.getPrincipal();
+        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt, GeneralUtils.buildUserInfo(localUser)));
+    }
 }
