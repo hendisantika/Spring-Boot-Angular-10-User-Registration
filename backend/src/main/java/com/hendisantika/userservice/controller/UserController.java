f2d939660fdd33ca574e20eb99c4721942349093
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-03 05:31:44 +0700

    #62 - Add UserController in Backend Service

---
+++ backend/src/main/java/com/hendisantika/userservice/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.hendisantika.userservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/11/20
+ * Time: 05.31
+ */
+@RestController
+@RequestMapping("/api")
+public class UserController {
+}
