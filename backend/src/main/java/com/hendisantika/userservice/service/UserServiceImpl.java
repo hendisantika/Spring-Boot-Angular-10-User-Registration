7dbac081c0dfde32e1fcc0c78a9ed38dcd9e42fb
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-03 05:20:48 +0700

    #49 - Add UserServiceImpl in Backend Service

--- backend/src/main/java/com/hendisantika/userservice/service/UserServiceImpl.java
+++ backend/src/main/java/com/hendisantika/userservice/service/UserServiceImpl.java
@@ -1,19 +1,29 @@
 package com.hendisantika.userservice.service;
 
+import com.hendisantika.userservice.dto.LocalUser;
+import com.hendisantika.userservice.dto.OAuth2UserInfo;
 import com.hendisantika.userservice.dto.SignUpRequest;
+import com.hendisantika.userservice.dto.SocialProvider;
 import com.hendisantika.userservice.entity.Role;
 import com.hendisantika.userservice.entity.User;
+import com.hendisantika.userservice.exception.OAuth2AuthenticationProcessingException;
 import com.hendisantika.userservice.exception.UserAlreadyExistAuthenticationException;
 import com.hendisantika.userservice.repository.RoleRepository;
 import com.hendisantika.userservice.repository.UserRepository;
+import com.hendisantika.userservice.util.GeneralUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -69,5 +79,49 @@ public class UserServiceImpl implements UserService {
         return user;
     }
 
+    @Override
+    public User findUserByEmail(final String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    @Transactional
+    public LocalUser processUserRegistration(String registrationId, Map<String, Object> attributes,
+                                             OidcIdToken idToken, OidcUserInfo userInfo) {
+        OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(registrationId, attributes);
+        if (StringUtils.isEmpty(oAuth2UserInfo.getName())) {
+            throw new OAuth2AuthenticationProcessingException("Name not found from OAuth2 provider");
+        } else if (StringUtils.isEmpty(oAuth2UserInfo.getEmail())) {
+            throw new OAuth2AuthenticationProcessingException("Email not found from OAuth2 provider");
+        }
+        SignUpRequest userDetails = toUserRegistrationObject(registrationId, oAuth2UserInfo);
+        User user = findUserByEmail(oAuth2UserInfo.getEmail());
+        if (user != null) {
+            if (!user.getProvider().equals(registrationId) && !user.getProvider().equals(SocialProvider.LOCAL.getProviderType())) {
+                throw new OAuth2AuthenticationProcessingException(
+                        "Looks like you're signed up with " + user.getProvider() + " account. Please use your " + user.getProvider() + " account to login.");
+            }
+            user = updateExistingUser(user, oAuth2UserInfo);
+        } else {
+            user = registerNewUser(userDetails);
+        }
+
+        return LocalUser.create(user, attributes, idToken, userInfo);
+    }
+
+    private User updateExistingUser(User existingUser, OAuth2UserInfo oAuth2UserInfo) {
+        existingUser.setDisplayName(oAuth2UserInfo.getName());
+        return userRepository.save(existingUser);
+    }
+
+    private SignUpRequest toUserRegistrationObject(String registrationId, OAuth2UserInfo oAuth2UserInfo) {
+        return SignUpRequest.getBuilder().addProviderUserID(oAuth2UserInfo.getId()).addDisplayName(oAuth2UserInfo.getName()).addEmail(oAuth2UserInfo.getEmail())
+                .addSocialProvider(GeneralUtils.toSocialProvider(registrationId)).addPassword("changeit").build();
+    }
+
+    @Override
+    public Optional<User> findUserById(Long id) {
+        return userRepository.findById(id);
+    }
 
 }
