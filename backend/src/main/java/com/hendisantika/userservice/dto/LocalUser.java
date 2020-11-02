a391b7d376120b673ef5e6fa95303754adf9bb92
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-03 05:21:32 +0700

    #50 - Add LocalUser DTO in Backend Service

--- backend/src/main/java/com/hendisantika/userservice/dto/LocalUser.java
+++ backend/src/main/java/com/hendisantika/userservice/dto/LocalUser.java
@@ -1,5 +1,16 @@
 package com.hendisantika.userservice.dto;
 
+import com.hendisantika.userservice.entity.User;
+import com.hendisantika.userservice.util.GeneralUtils;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : user-service
@@ -9,5 +20,71 @@ package com.hendisantika.userservice.dto;
  * Date: 31/10/20
  * Time: 22.56
  */
-public class LocalUser {
+public class LocalUser extends User implements OAuth2User, OidcUser {
+    private static final long serialVersionUID = -2845160792248762779L;
+    private final OidcIdToken idToken;
+    private final OidcUserInfo userInfo;
+    private final User user;
+    private Map<String, Object> attributes;
+
+    public LocalUser(final String userID, final String password, final boolean enabled,
+                     final boolean accountNonExpired, final boolean credentialsNonExpired,
+                     final boolean accountNonLocked, final Collection<? extends GrantedAuthority> authorities,
+                     final User user) {
+        this(userID, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities, user
+                , null, null);
+    }
+
+    public LocalUser(final String userID, final String password, final boolean enabled,
+                     final boolean accountNonExpired, final boolean credentialsNonExpired,
+                     final boolean accountNonLocked, final Collection<? extends GrantedAuthority> authorities,
+                     final User user, OidcIdToken idToken,
+                     OidcUserInfo userInfo) {
+        super(userID, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+        this.user = user;
+        this.idToken = idToken;
+        this.userInfo = userInfo;
+    }
+
+    public static LocalUser create(User user, Map<String, Object> attributes, OidcIdToken idToken,
+                                   OidcUserInfo userInfo) {
+        LocalUser localUser = new LocalUser(user.getEmail(), user.getPassword(), user.isEnabled(), true, true, true,
+                GeneralUtils.buildSimpleGrantedAuthorities(user.getRoles()),
+                user, idToken, userInfo);
+        localUser.setAttributes(attributes);
+        return localUser;
+    }
+
+    @Override
+    public String getName() {
+        return this.user.getDisplayName();
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return this.attributes;
+    }
+
+    public void setAttributes(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getClaims() {
+        return this.attributes;
+    }
+
+    @Override
+    public OidcUserInfo getUserInfo() {
+        return this.userInfo;
+    }
+
+    @Override
+    public OidcIdToken getIdToken() {
+        return this.idToken;
+    }
+
+    public User getUser() {
+        return user;
+    }
 }
