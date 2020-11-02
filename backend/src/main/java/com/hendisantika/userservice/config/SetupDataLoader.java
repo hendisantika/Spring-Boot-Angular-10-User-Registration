3302a8faa18e4e1044111ff51601eaa0535bb7f4
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-03 05:34:18 +0700

    #66 - Add onApplicationEvent in SetupDataLoader in Backend Service

--- backend/src/main/java/com/hendisantika/userservice/config/SetupDataLoader.java
+++ backend/src/main/java/com/hendisantika/userservice/config/SetupDataLoader.java
@@ -1,5 +1,6 @@
 package com.hendisantika.userservice.config;
 
+import com.hendisantika.userservice.entity.Role;
 import com.hendisantika.userservice.repository.RoleRepository;
 import com.hendisantika.userservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,9 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,4 +34,18 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Override
+    @Transactional
+    public void onApplicationEvent(final ContextRefreshedEvent event) {
+        if (alreadySetup) {
+            return;
+        }
+        // Create initial roles
+        Role userRole = createRoleIfNotFound(Role.ROLE_USER);
+        Role adminRole = createRoleIfNotFound(Role.ROLE_ADMIN);
+        Role modRole = createRoleIfNotFound(Role.ROLE_MODERATOR);
+        createUserIfNotFound("admin@javachinna.com", Set.of(userRole, adminRole, modRole));
+        alreadySetup = true;
+    }
 }
