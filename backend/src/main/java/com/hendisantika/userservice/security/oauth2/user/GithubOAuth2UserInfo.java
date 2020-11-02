756cd9d674059f5ea1aff63818c66768a5443a45
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-03 05:26:33 +0700

    #56 - Add GithubOAuth2UserInfo in Backend Service

---
+++ backend/src/main/java/com/hendisantika/userservice/security/oauth2/user/GithubOAuth2UserInfo.java
@@ -0,0 +1,39 @@
+package com.hendisantika.userservice.security.oauth2.user;
+
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/11/20
+ * Time: 05.26
+ */
+public class GithubOAuth2UserInfo extends OAuth2UserInfo {
+
+    public GithubOAuth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        return ((Integer) attributes.get("id")).toString();
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+
+    @Override
+    public String getImageUrl() {
+        return (String) attributes.get("avatar_url");
+    }
+}
