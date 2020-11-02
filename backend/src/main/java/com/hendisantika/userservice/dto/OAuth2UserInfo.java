8d1a1abc1d785be897c05aa2cc2eec2f8e15bb8d
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-03 05:21:51 +0700

    #51 - Add OAuth2UserInfo in Backend Service

---
+++ backend/src/main/java/com/hendisantika/userservice/dto/OAuth2UserInfo.java
@@ -0,0 +1,13 @@
+package com.hendisantika.userservice.dto;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/11/20
+ * Time: 05.15
+ */
+public class OAuth2UserInfo {
+}
