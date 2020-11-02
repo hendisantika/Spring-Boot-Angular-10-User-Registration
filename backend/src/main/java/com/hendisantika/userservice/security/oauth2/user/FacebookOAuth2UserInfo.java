7e5a95aaf775f7d00ebee9e9c71ecf6e7390187d
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-03 05:25:40 +0700

    #55 - Add FacebookOAuth2UserInfo in Backend Service

---
+++ backend/src/main/java/com/hendisantika/userservice/security/oauth2/user/FacebookOAuth2UserInfo.java
@@ -0,0 +1,48 @@
+package com.hendisantika.userservice.security.oauth2.user;
+
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/11/20
+ * Time: 05.25
+ */
+public class FacebookOAuth2UserInfo extends OAuth2UserInfo {
+    public FacebookOAuth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        return (String) attributes.get("id");
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public String getImageUrl() {
+        if (attributes.containsKey("picture")) {
+            Map<String, Object> pictureObj = (Map<String, Object>) attributes.get("picture");
+            if (pictureObj.containsKey("data")) {
+                Map<String, Object> dataObj = (Map<String, Object>) pictureObj.get("data");
+                if (dataObj.containsKey("url")) {
+                    return (String) dataObj.get("url");
+                }
+            }
+        }
+        return null;
+    }
+}
