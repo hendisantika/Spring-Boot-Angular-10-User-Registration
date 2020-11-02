0caec8f06324bd1f9addab1509adff50cdc1beaf
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-03 05:23:44 +0700

    #53 - Add OAuth2UserInfo in Backend Service

---
+++ backend/src/main/java/com/hendisantika/userservice/security/oauth2/user/OAuth2UserInfo.java
@@ -0,0 +1,32 @@
+package com.hendisantika.userservice.security.oauth2.user;
+
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/11/20
+ * Time: 05.23
+ */
+public abstract class OAuth2UserInfo {
+    protected Map<String, Object> attributes;
+
+    public OAuth2UserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    public abstract String getId();
+
+    public abstract String getName();
+
+    public abstract String getEmail();
+
+    public abstract String getImageUrl();
+}
