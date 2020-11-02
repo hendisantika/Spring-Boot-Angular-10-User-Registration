08cc515524e5bfd4d15d87f9675b22e4927e778f
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-03 05:24:54 +0700

    #54 - Add GoogleOAuth2UserInfo in Backend Service

---
+++ backend/src/main/java/com/hendisantika/userservice/security/oauth2/user/GoogleOAuth2UserInfo.java
@@ -0,0 +1,39 @@
+package com.hendisantika.userservice.security.oauth2.user;
+
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/11/20
+ * Time: 05.24
+ */
+public class GoogleOAuth2UserInfo extends OAuth2UserInfo {
+
+    public GoogleOAuth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        return (String) attributes.get("sub");
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+
+    @Override
+    public String getImageUrl() {
+        return (String) attributes.get("picture");
+    }
+}
