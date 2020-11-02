cd1d09c866bcb3cd2797938de3bcc0c7ee3992f9
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-03 05:33:08 +0700

    #64 - Add CurrentUser in Backend Service

---
+++ backend/src/main/java/com/hendisantika/userservice/config/CurrentUser.java
@@ -0,0 +1,25 @@
+package com.hendisantika.userservice.config;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/11/20
+ * Time: 05.32
+ */
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {
+}
